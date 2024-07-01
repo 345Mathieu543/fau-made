a225b5529a166f77f9a5c335bc93931037430656
345Mathieu543 <mathieu.stenzel@web.de>
2024-07-01 15:36:30 +0200

    Updated pipeline

--- project/pipeline3.py
+++ project/pipeline3.py
@@ -8,8 +8,9 @@ con = sqlite3.connect("../data/data.sqlite")
 # The average of the temperature and rain data is calculated and a column with row numbers is added to the tables
 # The two subqueries are joined together with thew table 'pedestrians' and the columns 'date', 'pedestrians', 'rain' and 'temp' are selected
 
-# Create a new table 'data' with the columns 'date', 'pedestrians', 'rain' and 'temp' into the database 'data.sqlite'
 p = con.cursor()
+
+# Create a new table 'data' with the columns 'date', 'pedestrians', 'rain' and 'temp' into the database 'data.sqlite'
 p.execute('''
 CREATE TABLE data (
     date TEXT,
@@ -18,10 +19,12 @@ CREATE TABLE data (
     temp REAL
 );
 ''')
+con.commit()
 
 # Insert the data into the table 'data'
 p.execute('''
-    WITH RainData AS (
+
+WITH RainData AS (
     SELECT
         r1.MESS_DATUM AS date,
         round((r1.[  RS] + r2.[  RS]) / 2, 2) AS rain
@@ -45,5 +48,20 @@ FROM pedestrians pd
 JOIN RainData rd ON pd.timestamp = rd.date
 JOIN TempData td ON pd.timestamp = td.date;
 ''')
+con.commit()
 
 print("[Pipeline3] Table 'data' has been created and the pedestrians data merged with the averaged weather data has been inserted.")
+
+# Update the string representation of the dates in the table 'data' to the format 'YYYY-MM-DD'
+p.execute('''
+
+UPDATE data
+SET date = SUBSTR(date, 1, 4) || '-' || SUBSTR(date, 5, 2) || '-' || SUBSTR(date, 7, 2)
+WHERE LENGTH(date) = 8;
+
+''')
+con.commit()
+
+print("[Pipeline3] The String representation of the dates in the table 'data' has been updated.")
+
+p.close()
